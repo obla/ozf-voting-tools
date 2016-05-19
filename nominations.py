dc77c24f2495cee27ad76846185df83585e115d8
Kenneth Anderson <ken@kenneth.net.nz>
2016-05-20 09:56:29 +1200

    initial commit of nominations.py

---
+++ nominations.py
@@ -0,0 +1,116 @@
+import argparse
+import json
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file')
+    args = parser.parse_args()
+
+    nominations_list = read_nominations_file(args.file)
+    teams = create_team_objects(nominations_list)
+    divisions = sort_teams(teams)
+    for name, teams in sorted(divisions.items()):
+        create_division_report(name, teams)
+
+
+def read_nominations_file(filename):
+    with open(filename, 'r') as nominations_file:
+        nominations_list = nominations_file.readlines()
+
+    nominations_list = [line.strip()
+                        for line in nominations_list
+                        if line != '\n'
+                        and line != '']
+
+    return nominations_list
+
+def create_team_objects(nominations_list):
+    current_team = ''
+    teams = {}
+
+    for line in nominations_list:
+        heading, data = tuple(line.split(':'))
+        heading = heading.strip()
+        data = data.strip()
+
+        if 'Team Name' in heading:
+            current_team = data
+            teams[current_team] = {}
+        elif 'Division' in heading:
+            if 'open' in data.lower():
+                teams[current_team]['division'] = 'open'
+            if 'intermediate' in data.lower():
+                teams[current_team]['division'] = 'intermediate'
+            if 'prem' in data.lower():
+                teams[current_team]['division'] = 'premier'
+        elif 'Best Scout' in heading:
+            if 'scouts' in teams[current_team]:
+                teams[current_team]['scouts'] += [data]
+            else:
+                teams[current_team]['scouts'] = [data]
+        elif 'Best Pocket' in heading:
+            teams[current_team]['pocket'] = data
+        elif 'Best Roaming' in heading:
+            teams[current_team]['roamer'] = data
+        elif 'Best Medic' in heading:
+            teams[current_team]['medic'] = data
+        elif 'Best Demoman' in heading:
+            teams[current_team]['demo'] = data
+        elif 'Best Utility' in heading:
+            teams[current_team]['utility'] = data
+        elif 'Most Improved' in heading:
+            teams[current_team]['most improved'] = data
+
+    return teams
+
+def sort_teams(teams):
+    divisions = {}
+    for team_name, data in teams.items():
+        team_division = data['division']
+        if team_division not in divisions:
+            divisions[team_division] = {}
+        del data['division']
+        divisions[team_division][team_name] = data
+
+    return divisions
+
+def create_division_report(name, teams):
+    awards = {
+        'Best Scout': 'scouts',
+        'Best Utility': 'utility',
+        'Best Pocket': 'pocket',
+        'Best Roamer': 'roamer',
+        'Best Demoman': 'demo',
+        'Best Medic': 'medic',
+        'Most Improved Player': 'most improved',
+        'Most Improved Team': 'team award',
+        'Friendliest Team': 'team award'
+    }
+
+    for award, designator in awards.items():
+        # print(award + ': ', end='')
+        if designator == 'team award':
+            canidate_list = list(teams.keys())
+        else:
+            canidate_list = get_award_list(teams, designator)
+        awards[award] = canidate_list
+        # print(*canidate_list, sep=', ')
+
+    print(name.capitalize(), 'voting')
+    print('=' * len(name + ' voting'))
+
+    for award, canidate_list in sorted(awards.items()):
+        print(award + ': ', end='')
+        print(*canidate_list, sep=', ', end='\n\n')
+
+    print()
+
+
+def get_award_list(teams, designator):
+    class_list = [team[designator] for team in teams.values() if designator in team]
+
+    if type(class_list[0]) is list:
+        class_list = [scout for scout_list in class_list for scout in scout_list]
+    return class_list
+
+main()
